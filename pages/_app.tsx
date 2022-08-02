3115f491fab4bd76bbfc9acb32e2d27a91781851
shakilkhan12 <shakilkhan621@gmail.com>
2022-08-02 10:40:54 +0500

    changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import axios from 'axios'
 import NextNProgress from "nextjs-progressbar";
-axios.defaults.baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_ENV === 'production' ? process.env.NEXT_PUBLIC_REMOTE_URL :  process.env.NEXT_PUBLIC_BASE_URL;
 axios.defaults.withCredentials = true;
 
 function MyApp({ Component, pageProps }: AppProps) {
