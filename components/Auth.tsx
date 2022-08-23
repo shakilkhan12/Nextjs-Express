2fc16cdddb300d42be860f120e94c1a0aac81687
shakilkhan12 <shakilkhan621@gmail.com>
2022-08-23 11:04:30 +0500

    auth

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,18 +1,21 @@
-import { useSession } from "../context/AuthProvider"
-import { useEffect } from "react"
+import { useContext, useEffect } from "react"
+import AuthContext, { useSession } from "../context/AuthProvider";
 import { useRouter } from "next/router";
+const Auth = (Component: any) => {
+  // const {auth, loading} = useSession();
+  const {auth, loading} = useContext(AuthContext)
+  console.log(auth, loading)
+      const router = useRouter();
+      useEffect(() => {
+        console.log('use effect')
+        if(!loading && !auth.auth) {  
+          router.push('/login')
+        }
+      }, [auth,loading])
+     
+     const ComponentWrapper = (props: any) => auth.auth && <Component {...props} />  
 
-const Auth = (Component: any) => (props: any) => {
-  const {auth, loading} = useSession();
-  const router = useRouter();
-  console.log('High order component', auth.auth)
-  useEffect(() => {
-    console.log('use effect')
-    if(!loading && !auth.auth) {  
-      router.push('/login')
-    }
-  }, [auth,loading])
-  return auth.auth && <Component {...props} /> 
+     return <ComponentWrapper/>
 }
 
 export default Auth
